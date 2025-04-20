a991423d4cc69edb24e20cf46cceaf72c0c002df
IgorMichels <igorxadrezbr@gmail.com>
2025-04-20 17:45:55 -0300

    added constants'

---
+++ scripts/constants.py
@@ -0,0 +1,22 @@
+N_CLUBS = 20
+N_SEASONS = 10
+
+CHAINS = 4
+ITER_WARMUP = 100
+ITER_SAMPLING = 100
+
+ADAPT_DELTA = 0.8
+MAX_TREE_DEPTH = 10
+
+generator_kwargs = {
+    'n_clubs': N_CLUBS,
+    'n_seasons': N_SEASONS,
+}
+
+model_kwargs = {
+    'chains': CHAINS,
+    'iter_warmup': ITER_WARMUP,
+    'iter_sampling': ITER_SAMPLING,
+    'adapt_delta': ADAPT_DELTA,
+    'max_treedepth': MAX_TREE_DEPTH,
+}
