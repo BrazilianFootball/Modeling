86d6003b1b6661df3eea3920d7f6b56d28bf09ca
IgorMichels <igorxadrezbr@gmail.com>
2025-04-24 00:36:00 -0300

    run black

--- scripts/bradley_terry_1.py
+++ scripts/bradley_terry_1.py
@@ -1,17 +1,10 @@
+from constants import ITER_SAMPLING, ITER_WARMUP, N_SIMS, generator_kwargs, model_kwargs
 from generators import data_generator_bt_1
 from utils import run_model
-from constants import (
-    generator_kwargs,
-    model_kwargs,
-    N_SIMS,
-    ITER_WARMUP,
-    ITER_SAMPLING,
-)
-
 
 if __name__ == "__main__":
-    model_name = "bt_model_1"
+    MODEL_NAME = "bt_model_1"
     generator = data_generator_bt_1
-    model_kwargs["iter_warmup"] = ITER_WARMUP[model_name]
-    model_kwargs["iter_sampling"] = ITER_SAMPLING[model_name]
-    run_model(model_name, N_SIMS, generator, generator_kwargs, model_kwargs)
+    model_kwargs["iter_warmup"] = ITER_WARMUP[MODEL_NAME]
+    model_kwargs["iter_sampling"] = ITER_SAMPLING[MODEL_NAME]
+    run_model(MODEL_NAME, N_SIMS, generator, generator_kwargs, model_kwargs)
--- scripts/constants.py
+++ scripts/constants.py
@@ -4,29 +4,29 @@ N_SEASONS = 1
 
 CHAINS = 4
 ITER_WARMUP = {
-    'bt_model_1': 1_000,
-    'bt_model_2': 1_000,
-    'poisson_model_1': 1_000,
-    'poisson_model_2': 1_000,
+    "bt_model_1": 1_000,
+    "bt_model_2": 1_000,
+    "poisson_model_1": 1_000,
+    "poisson_model_2": 1_000,
 }
 
 ITER_SAMPLING = {
-    'bt_model_1': 1_000,
-    'bt_model_2': 1_000,
-    'poisson_model_1': 1_000,
-    'poisson_model_2': 1_000,
+    "bt_model_1": 1_000,
+    "bt_model_2": 1_000,
+    "poisson_model_1": 1_000,
+    "poisson_model_2": 1_000,
 }
 
 ADAPT_DELTA = 0.8
 MAX_TREE_DEPTH = 10
 
 generator_kwargs = {
-    'n_clubs': N_CLUBS,
-    'n_seasons': N_SEASONS,
+    "n_clubs": N_CLUBS,
+    "n_seasons": N_SEASONS,
 }
 
 model_kwargs = {
-    'chains': CHAINS,
-    'adapt_delta': ADAPT_DELTA,
-    'max_treedepth': MAX_TREE_DEPTH,
+    "chains": CHAINS,
+    "adapt_delta": ADAPT_DELTA,
+    "max_treedepth": MAX_TREE_DEPTH,
 }
