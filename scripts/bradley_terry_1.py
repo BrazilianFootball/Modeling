a028507b5d431356db416f02ffa37a96c5b504b0
IgorMichels <igorxadrezbr@gmail.com>
2025-04-23 23:11:40 -0300

    nail-care

--- scripts/bradley_terry_1.py
+++ scripts/bradley_terry_1.py
@@ -8,9 +8,10 @@ from constants import (
     ITER_SAMPLING,
 )
 
+
 if __name__ == "__main__":
     model_name = "bt_model_1"
     generator = data_generator_bt_1
-    model_kwargs['iter_warmup'] = ITER_WARMUP[model_name]
-    model_kwargs['iter_sampling'] = ITER_SAMPLING[model_name]
+    model_kwargs["iter_warmup"] = ITER_WARMUP[model_name]
+    model_kwargs["iter_sampling"] = ITER_SAMPLING[model_name]
     run_model(model_name, N_SIMS, generator, generator_kwargs, model_kwargs)
