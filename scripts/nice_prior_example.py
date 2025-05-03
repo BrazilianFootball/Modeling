9efebcc3c98515ba1eabde23f276eceb84fbd085
IgorMichels <igorxadrezbr@gmail.com>
2025-05-03 02:22:58 -0300

    change n_observations per simulation

--- scripts/nice_prior_example.py
+++ scripts/nice_prior_example.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     MODEL_NAME = "nice_prior_example"
     N_SIMS = 100
 
-    generator_kwargs = {"n_observations": 5, "true_mu": 0, "true_sigma": 1}
+    generator_kwargs = {"n_observations": 1, "true_mu": 0, "true_sigma": 1}
     model_kwargs["iter_warmup"] = ITER_WARMUP[MODEL_NAME]
     model_kwargs["iter_sampling"] = ITER_SAMPLING[MODEL_NAME]
     run_model(
