e8a7851633a3f6873c425a12d39f624b05725fe4
IgorMichels <igorxadrezbr@gmail.com>
2025-06-25 00:08:23 -0300

    update constants

--- src/features/constants.py
+++ src/features/constants.py
@@ -1,26 +1,29 @@
-N_SIMS = 1_000
+N_SIMS = 250
 N_CLUBS = 20
 N_SEASONS = 1
 
+DEFAULT_ITER_WARMUP = 500
+DEFAULT_ITER_SAMPLING = 500
+
 CHAINS = 4
 ITER_WARMUP = {
-    "bt_model_1": 2_000,
-    "bt_model_2": 2_000,
-    "poisson_model_1": 2_000,
-    "poisson_model_2": 2_000,
-    "karlis_ntzoufras_1": 2_000,
-    "karlis_ntzoufras_2": 2_000,
+    "bt_model_1": DEFAULT_ITER_WARMUP,
+    "bt_model_2": DEFAULT_ITER_WARMUP,
+    "poisson_model_1": DEFAULT_ITER_WARMUP,
+    "poisson_model_2": DEFAULT_ITER_WARMUP,
+    "karlis_ntzoufras_1": DEFAULT_ITER_WARMUP,
+    "karlis_ntzoufras_2": DEFAULT_ITER_WARMUP,
     "bad_prior_example": 100,
     "nice_prior_example": 100,
 }
 
 ITER_SAMPLING = {
-    "bt_model_1": 2_000,
-    "bt_model_2": 2_000,
-    "poisson_model_1": 2_000,
-    "poisson_model_2": 2_000,
-    "karlis_ntzoufras_1": 2_000,
-    "karlis_ntzoufras_2": 2_000,
+    "bt_model_1": DEFAULT_ITER_SAMPLING,
+    "bt_model_2": DEFAULT_ITER_SAMPLING,
+    "poisson_model_1": DEFAULT_ITER_SAMPLING,
+    "poisson_model_2": DEFAULT_ITER_SAMPLING,
+    "karlis_ntzoufras_1": DEFAULT_ITER_SAMPLING,
+    "karlis_ntzoufras_2": DEFAULT_ITER_SAMPLING,
     "bad_prior_example": 100,
     "nice_prior_example": 100,
 }
