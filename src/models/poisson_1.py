bff1d60681381d5ef7705d922cd660c8800c64f7
IgorMichels <igorxadrezbr@gmail.com>
2025-08-11 00:11:37 -0300

    added poisson models on python

--- src/models/poisson_1.py
+++ src/models/poisson_1.py
@@ -9,7 +9,7 @@ from src.features.generators import data_generator_poisson_1
 from src.features.utils import run_model
 
 if __name__ == "__main__":
-    MODEL_NAME = "poisson_model_1"
+    MODEL_NAME = "poisson_1"
     generator = data_generator_poisson_1
     model_kwargs["iter_warmup"] = ITER_WARMUP[MODEL_NAME]
     model_kwargs["iter_sampling"] = ITER_SAMPLING[MODEL_NAME]
--- src/models/poisson_3.py
+++ src/models/poisson_3.py
@@ -5,12 +5,12 @@ from src.features.constants import (
     generator_kwargs,
     model_kwargs,
 )
-from src.features.generators import data_generator_poisson_2
+from src.features.generators import data_generator_poisson_3
 from src.features.utils import run_model
 
 if __name__ == "__main__":
-    MODEL_NAME = "poisson_2"
-    generator = data_generator_poisson_2
+    MODEL_NAME = "poisson_3"
+    generator = data_generator_poisson_3
     model_kwargs["iter_warmup"] = ITER_WARMUP[MODEL_NAME]
     model_kwargs["iter_sampling"] = ITER_SAMPLING[MODEL_NAME]
     run_model(MODEL_NAME, N_SIMS, generator, generator_kwargs, model_kwargs)
